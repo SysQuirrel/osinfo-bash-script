0567795de3f83a04ace93d7e294303d5a62b413b
SysQuirrel <86571275+SysQuirrel@users.noreply.github.com>
2025-08-21 00:09:34 +0530

    made the script to get the system kernel and os infos

--- resource.sh
+++ resource.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 os=$(hostnamectl |grep '^Operating System')
-archi=$(hostnamectl |grep '[[:blank:]]*Architecture')
-echo $os
-echo $archi
+archi=$(hostnamectl | grep '[[:blank:]]*Architecture' | sed 's/    //')
+##archi_type=$(uname -m)
+echo "##############"
+echo "$os"
+echo "$archi"
+echo "Current user is : $(whoami)"
+echo "System name is : $(hostname)"
+echo "The current date and time is : $(date)"
+echo "The system is running $(uptime -p | grep -o 'up [^,]*' | cut -d' ' -f 2-)"
+echo "##############"
+
+
+echo "$(lscpu | grep '^CPU(s)')"
+echo "CPU $(lscpu | grep '^Vendor ID')"
+
