58e54c31b65ec712147b93203f6f5adf11749ee3
SysQuirrel <86571275+SysQuirrel@users.noreply.github.com>
2025-09-20 01:30:00 +0530

    - Added the funtion to show how many users are there on the system - Edited few other functions with better retrieval processes to get the cpu infos

--- resource.sh
+++ resource.sh
@@ -5,13 +5,14 @@ archi=$(hostnamectl | grep '[[:blank:]]*Architecture' | sed 's/    //')
 echo "##############"
 echo "$os"
 echo "$archi"
+echo "All users on the system is/are: $(who | cut -d' ' -f1 | uniq)" 
 echo "Current user is : $(whoami)"
 echo "System name is : $(hostname)"
 echo "The current date and time is : $(date)"
-echo "The system is running $(uptime -p | grep -o 'up [^,]*' | cut -d' ' -f 2-)"
+echo "The system is running for $(uptime -p | grep -o 'up [^,]*' | cut -d' ' -f 2-)"
 echo "##############"
 
 
-echo "$(lscpu | grep '^CPU(s)')"
-echo "CPU $(lscpu | grep '^Vendor ID')"
+echo "Number of CPUs: $(nproc --all)"
+echo "CPU manufacturer: $(lscpu | grep '^Vendor ID' | awk '{print $3}')"
 
